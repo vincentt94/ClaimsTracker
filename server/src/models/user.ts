9add57b123240cc114e1ab960f7bfb012268d80c
vincentt94 <vincent.h.thao@gmail.com>
2025-03-29 21:55:22 -0500

    updated user model & signtoken.ts in utils folder

---
+++ server/src/models/user.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+// Password comparison method
+userSchema.methods.isCorrectPassword = function (password: string) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
